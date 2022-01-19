29944c329edbb8909729feaeabe36cce3cb63b7c
gitnoober <priyanshusingh349@gmail.com>
2022-01-19 23:02:21 +0530

    settup env vars

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -23,14 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-de&#tdyglrvz67%z5k=$givc#3ut%@4nj-6*ww(uo5a=fv8*c4'
-# SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
-SECRET_KEY= 'django-insecure-de&#tdyglrvz67%z5k=$givc#3ut%@4nj-6*ww(uo5a=fv8*c4'
-
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv("DEBUG", "False") == "True"
-DEBUG = False
-
+DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ['*']
 
 
